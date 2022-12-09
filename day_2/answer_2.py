6454da68f32b4af0699af8942466e15d7d5783ef
Stephan <stephan.semerad@gmail.com>
2022-12-09 09:52:18 +0100

    day 2 completed

--- day_2/answer_2.py
+++ day_2/answer_2.py
@@ -1,34 +1,40 @@
-lines = open('input.txt', 'r').readlines()
+lines = open("input.txt", "r").readlines()
 
-first_score = {'Rock': 1, 'Paper': 2, 'Scissors': 3}
-second_score = {'Lose': 0, 'Draw': 3, 'Win': 6}
+first_score = {"Rock": 1, "Paper": 2, "Scissors": 3}
+second_score = {"Lose": 0, "Draw": 3, "Win": 6}
 
-player_a = {'A': 'Rock', 'B': 'Paper', 'C': 'Scissors'}
-player_b = {'X': 'Lose', 'Y': 'Draw', 'Z': 'Win'}
+player_a = {"A": "Rock", "B": "Paper", "C": "Scissors"}
+player_b = {"X": "Lose", "Y": "Draw", "Z": "Win"}
 
 score = 0
 for line in lines:
-    player_a_choice, result= list(line)[0], list(line)[2]
-    player_a_choice, result= player_a[player_a_choice], player_b[result]
-    print('player_a_choice: ', player_a_choice, 'result', result)
+    player_a_choice, result = list(line)[0], list(line)[2]
+    player_a_choice, result = player_a[player_a_choice], player_b[result]
+    print("player_a_choice: ", player_a_choice, "result", result)
 
-    if result == 'Draw':
+    player_b_choice = ""
+
+    if result == "Win":
+        if player_a_choice == "Rock":
+            player_b_choice = "Paper"
+        if player_a_choice == "Paper":
+            player_b_choice = "Scissors"
+        if player_a_choice == "Scissors":
+            player_b_choice = "Rock"
+
+    if result == "Lose":
+        if player_a_choice == "Paper":
+            player_b_choice = "Rock"
+        if player_a_choice == "Scissors":
+            player_b_choice = "Paper"
+        if player_a_choice == "Rock":
+            player_b_choice = "Scissors"
+
+    if result == "Draw":
         player_b_choice = player_a_choice
-        score = first_score['player_b_choice']
-        print('player_b_choice is {player_a_choice}')
+
+    score += second_score[result]
+    score += first_score[player_b_choice]
 
 
-    if result == 'Win':
-        if player_a_choice == 'Rock':
-        if player_a_choice == 'Paper':
-        if player_a_choice == 'Scissors':
-
-
-
-
-#     result = player_b[player_b_choice]
-#     print('result: ', result, ' player_a_choice: ', player_a_choice)
-
-
-
-# print('score: ', score)
+print("score: ", score)
