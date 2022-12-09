6b7d4e36d0b85c1c0f9cc6264cedb84cfe3e204f
Stephan <stephan.semerad@gmail.com>
2022-12-09 22:31:32 +0100

    test

--- day_4/answer_1.py
+++ day_4/answer_1.py
@@ -1,43 +1,24 @@
 lines = open("input.txt", "r").readlines()
 
 example_lines = ["2-4,6-8", "2-3,4-5", "5-7,7-9", "2-8,3-7", "6-6,4-6", "2-6,4-8"]
-lines = example_lines
 
 
 def convert_to_range(group):
     section_1, section_2 = group.split("-")
     group_range = list(range(int(section_1), int(section_2) + 1))
-    return group_range
+    return list(set(group_range))
 
 
 score = 0
 for line in lines:
     group_1, group_2 = line.split(",")
     group_1, group_2 = convert_to_range(group_1), convert_to_range(group_2)
+    comparison = list(set(group_1).intersection(set(group_2)))
+    print(group_1, group_2, comparison)
 
-    scoring = False
-
-    for job in group_1:
-        if job in group_2:
-            scoring = True
-        else:
-            scoring = False
-
-    for job in group_2:
-        if job in group_1:
-            scoring = True
-        else:
-            scoring = False
-
-    if scoring:
+    if comparison == group_1 or comparison == group_2:
+        print("one_score")
         score += 1
-
-    # comparison = list(set(group_1).intersection(set(group_2)))
-    # print(group_1, group_2, comparison)
-
-    # if comparison == group_1 or comparison == group_2:
-    #     print("one_score")
-    #     score += 1
 
     # print("")
 
